0391094651eb889a74066f7fc65d0132052257d9
Corey Lutrick <coreylutrick@gmail.com>
2018-04-03 18:33:52 -0700

    created hero cards

--- javaScripts/superheroes.js
+++ javaScripts/superheroes.js
@@ -1,1 +1,50 @@
-console.log("poop")
+const PrintToDom = (domString, divId) => 
+{
+  document.getElementById(divId).innerHTML = domString;
+};
+
+const buildDomString = (heroArray) => 
+{
+  let domString = ""
+  heroArray.forEach((superheroes) =>
+  {
+    domString += `<div class="col-md-3">`
+    domString +=  `<div class="panel">`
+    domString +=    `<div class="panel-heading">`
+    domString +=      `<h3 class="panel-title">${superheroes.name}</h3>`
+    domString +=    `</div>`
+    domString +=    `<div class="panel-body">`
+    domString +=      `<img class="heroImg" src="${superheroes.image}"<br>`
+    domString +=      `<p class="heroDesc">${superheroes.description}</p>`
+    domString +=    `</div>`
+    domString +=  `</div>`
+    domString += `</div>`
+  });
+  PrintToDom(domString, "hero-holder")
+};
+
+function executeThisCodeAfterFileLoaded () 
+{
+  const data = JSON.parse(this.responseText);
+  buildDomString(data.superheroes);
+}
+
+
+function executeThisCodeIfXHRFails () 
+{
+  console.log("something went wrong");
+}
+
+
+const startApp = () => 
+{
+  let myRequest = new XMLHttpRequest();
+  myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+  myRequest.addEventListener("error", executeThisCodeIfXHRFails);
+  myRequest.open("get", "../db/superheroes.json");
+  myRequest.send();
+};
+
+
+
+startApp();
